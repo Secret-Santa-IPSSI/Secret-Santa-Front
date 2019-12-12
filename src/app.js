a6cee7d8d7684ce5af6748864da3908169af4997
BuyukCem <cembuyuk7@gmail.com>
2019-12-12 23:01:25 +0100

    ADD new routes AND views

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const express = require('express');
-const serv= require('./config/confServeur.js');
+const Confserv= require('./config/confServeur.js');
 
 const app = express();
 var routes=require('./routes/index');
 var group= require('./routes/group');
+var addgroup=require('./routes/addgroup');
+var people=require('./routes/people');
+var addPeople=require('./routes/addpeople');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -12,6 +15,9 @@ app.use(express.static(path.join(__dirname,'/public')));
 
 app.use('/', routes);
 app.use('/group',group);
+app.use('/addgroup',addgroup);
+app.use('/people', people);
+app.use('/addPeople', addPeople);
 
 app.use((req, res, next) => {
   res.status(404).render("erreur");
@@ -22,6 +28,6 @@ app.use((req, res, next) => {
 })
     // console.error(err.stack);
     //res.status(500).send('Something broke!');
-.listen(serv.local.port, () => {
-    console.log(`App running at http://localhost:`+serv.local.port);
+.listen(Confserv.local.port, () => {
+    console.log(`App running at http://localhost:`+Confserv.local.port);
 });
